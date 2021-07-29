192690bd83c130d48b5ec200f258dd878def99a5
Mya Pitzeruse <mya@effx.com>
2021-07-29 15:07:16 -0500

    fix leak

--- internal/plugins/kubernetes/pods/heartbeat.go
+++ internal/plugins/kubernetes/pods/heartbeat.go
@@ -9,16 +9,21 @@ import (
 	"github.com/jonboulle/clockwork"
 )
 
+type item struct {
+	cancelled bool
+	timestamp int64
+}
+
 type HeartbeatFunc func(ctx context.Context, id string)
 
 func NewHeartbeat(period time.Duration, fn HeartbeatFunc) *Heartbeat {
 	return &Heartbeat{
-		Clock:      clockwork.NewRealClock(),
-		period:     period,
-		fn:         fn,
-		mu:         &sync.Mutex{},
-		timestamps: make(map[string]int64),
-		queue:      make([]string, 0),
+		Clock:  clockwork.NewRealClock(),
+		period: period,
+		fn:     fn,
+		mu:     &sync.Mutex{},
+		items:  make(map[string]*item),
+		queue:  make([]string, 0),
 	}
 }
 
@@ -29,15 +34,70 @@ type Heartbeat struct {
 	period time.Duration
 	fn     HeartbeatFunc
 
-	mu         *sync.Mutex
-	timestamps map[string]int64
-	queue      []string
+	mu    *sync.Mutex
+	items map[string]*item
+	queue []string
+}
+
+// Dequeue removes the heartbeat associated with the id from the list. This is roughly a log(n) runtime. There is _some_
+// scanning involved, but it's only in the event that two heartbeats are scheduled at the same time.
+func (h *Heartbeat) Dequeue(id string) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	if _, ok := h.items[id]; !ok {
+		// doesn't exist
+		return
+	}
+
+	h.items[id].cancelled = true
+}
+
+// Enqueue adds the provided id to the list of elements to process. If runNow is true, then the element is enqueued
+// using the current timestamp rather than one in the future. This operation should be close to log(n) runtime.
+func (h *Heartbeat) Enqueue(id string) {
+	if e := h.items[id]; e != nil {
+		return
+	}
+
+	h.enqueue(id)
+}
+
+func (h *Heartbeat) enqueue(id string) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	if _, ok := h.items[id]; !ok {
+		h.items[id] = &item{
+			cancelled: false,
+			timestamp: 0,
+		}
+	}
+
+	if h.items[id].cancelled {
+		delete(h.items, id)
+	} else {
+		timeForID := h.Clock.Now().Add(h.period).UnixNano()
+
+		pos := sort.Search(len(h.queue), func(i int) bool {
+			return timeForID < h.items[h.queue[i]].timestamp
+		})
+
+		h.items[id].timestamp = timeForID
+		if pos == len(h.queue) {
+			h.queue = append(h.queue, id)
+		} else {
+			h.queue = append(h.queue[:pos], append([]string{id}, h.queue[pos:]...)...)
+		}
+	}
 }
 
 func (h *Heartbeat) heartbeat(ctx context.Context, id string) {
-	// ensure we re-enqueue for later processing when we're done
-	defer h.Enqueue(id, false)
-	h.fn(ctx, id)
+	defer h.enqueue(id)
+
+	if !h.items[id].cancelled {
+		h.fn(ctx, id)
+	}
 }
 
 // Poll reads from the head of the queue until we reach an entry with a greater timestamp.
@@ -48,17 +108,11 @@ func (h *Heartbeat) Poll(ctx context.Context) {
 	now := h.Clock.Now().UnixNano()
 	i := 0
 
-	for ; h.timestamps[h.queue[i]] <= now; i++ {
+	for ; h.items[h.queue[i]].timestamp <= now; i++ {
 		go h.heartbeat(ctx, h.queue[i])
 	}
 
-	ids := h.queue[:i]
-
-	// "pop" off processed ids
 	h.queue = h.queue[i:]
-	for _, id := range ids {
-		delete(h.timestamps, id)
-	}
 }
 
 // Start forks a go routine until the provided context is cancelled. Every 200 ms, it attempts to poll the underlying
@@ -77,62 +131,4 @@ func (h *Heartbeat) Start(ctx context.Context) {
 			}
 		}
 	}()
-}
-
-// Dequeue removes the heartbeat associated with the id from the list. This is roughly a log(n) runtime. There is _some_
-// scanning involved, but it's only in the event that two heartbeats are scheduled at the same time.
-func (h *Heartbeat) Dequeue(id string) {
-	h.mu.Lock()
-	defer h.mu.Unlock()
-
-	timeForID, ok := h.timestamps[id]
-	if !ok {
-		return
-	}
-
-	pos := sort.Search(len(h.queue), func(i int) bool {
-		// smallest index where this is true
-		return timeForID <= h.timestamps[h.queue[i]]
-	})
-
-	for {
-		if h.queue[pos] == id {
-			// found it! prune
-			h.queue = append(h.queue[0:pos], h.queue[pos+1:]...)
-			delete(h.timestamps, id)
-			return
-
-		} else if h.timestamps[h.queue[pos]] > timeForID {
-			panic("expected entry to exist, but it doesn't appear to. system in a bad state")
-		}
-
-		pos++
-	}
-}
-
-// Enqueue adds the provided id to the list of elements to process. If runNow is true, then the element is enqueued
-// using the current timestamp rather than one in the future. This operation should be close to log(n) runtime.
-func (h *Heartbeat) Enqueue(id string, runNow bool) {
-	// dequeue to ensure the element is not already queued.
-	h.Dequeue(id)
-
-	h.mu.Lock()
-	defer h.mu.Unlock()
-
-	now := h.Clock.Now()
-	if !runNow {
-		now = now.Add(h.period)
-	}
-	timeForID := now.UnixNano()
-
-	pos := sort.Search(len(h.queue), func(i int) bool {
-		return timeForID < h.timestamps[h.queue[i]]
-	})
-
-	h.timestamps[id] = timeForID
-	if pos == len(h.queue) {
-		h.queue = append(h.queue, id)
-	} else {
-		h.queue = append(h.queue[:pos], append([]string{id}, h.queue[pos:]...)...)
-	}
 }
